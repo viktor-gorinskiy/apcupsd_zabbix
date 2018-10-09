9068ea6598656318809193234ccfd2ac328f2eb8
viktor-gorinskiy <31724977+viktor-gorinskiy@users.noreply.github.com>
2018-10-09 18:34:09 +0700

    Update apcups-start.sh

--- apcupsd/scripts/apcups-start.sh
+++ apcupsd/scripts/apcups-start.sh
@@ -8,5 +8,5 @@ UPS_DAEMON="/sbin/apcupsd"
 for CONF_FILE in /etc/apcupsd/apcupsd-*conf
 do
     $UPS_DAEMON -f $CONF_FILE
-#    echo $UPS_DAEMON -f $CONF_FILE
+    echo $UPS_DAEMON -f $CONF_FILE
 done
