ef25e9ee5cd2bcf3a971013e9694895a857c6bd1
viktor-gorinskiy <31724977+viktor-gorinskiy@users.noreply.github.com>
2018-10-10 17:05:06 +0700

    Update enable_ups.sh

--- apcupsd/scripts/enable_ups.sh
+++ apcupsd/scripts/enable_ups.sh
@@ -4,18 +4,16 @@ conf_files="/etc/apcupsd/apcupsd-*.conf"
 
 if [ -f $conf_file ]
 then
-printf "{\"data\":["
+    printf "{\"data\":["
 
-for conf_file in $conf_files
-do
-ups_serial_number=$(printf ${conf_file%.conf}| grep -E -o '[^-]+$')
-
-printf $var"{\"{#UPS}\":\"$ups_serial_number\"}"
-
-var=","
-done
-printf "]}"
+    for conf_file in $conf_files
+        do
+        ups_serial_number=$(printf ${conf_file%.conf}| grep -E -o '[^-]+$')
+        ups_name=$(/usr/bin/awk '/UPSNAME/{print$2}' $conf_file)
+        printf $var"{\"{#UPS}\":\"$ups_name\"}"
+        var=","
+        done
+    printf "]}"
 
 fi
 exit 0
-
